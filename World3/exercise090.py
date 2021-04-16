65133259b642f7135fa17eae6de9d763a2b0a64b
Hugo <hugolerib@gmail.com>
2021-04-16 14:22:51 -0300

    Exercise 90 - World 3

---
+++ World3/exercise090.py
@@ -0,0 +1,16 @@
+# Author: Hugo Leça Ribeiro
+# Exercise 090: Dictionary in Python
+# Make a program that reads the name and average score of a student
+# Storage too the situation in a dictionary
+# At the end, show the contain
+
+student = dict()
+
+student['Name'] = input('Name: ')
+student['Average'] = float(input(f'Average of the student {student["Name"]}: '))
+if student['Average'] >= 7:
+    print('Approved')
+elif 3 <= student['Average'] < 7:
+    print('Recovery')
+else:
+    print('Reproved')
